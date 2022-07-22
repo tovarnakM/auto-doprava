00dd6d96c1b10b16d8a17127e749adeabce3e652
martintovarnak <tovarnakM@gmail.com>
2022-07-23 00:08:40 +0200

    Fix width

--- client/src/sharedComponents/Layout.js
+++ client/src/sharedComponents/Layout.js
@@ -4,7 +4,7 @@ import { Box } from 'components/Box'
 
 export const Layout = ({ children }) => (
   <Box px={{ _: rem(10) }} display="flex" justifyContent="center">
-    <Box width="10%" maxWidth={rem(1300)}>
+    <Box width="100%" maxWidth={rem(1300)}>
       {children}
     </Box>
   </Box>
